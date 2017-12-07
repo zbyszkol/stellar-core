006fcc218693cdf0a9a25fc21d383d02098abb67
Łukasz Lachowski <lukasz@stellar.org>
2017-12-07 16:57:08 +0100

    Modifed BenchmarkTests.cpp: removed config initialization (using "test" config).

--- src/simulation/BenchmarkTests.cpp
+++ src/simulation/BenchmarkTests.cpp
@@ -13,37 +13,13 @@
 
 using namespace stellar;
 
-std::unique_ptr<Config>
-initializeConfig()
-{
-    std::unique_ptr<Config> cfg = make_unique<Config>(getTestConfig());
-    // cfg->DATABASE = SecretValue{"postgresql://dbname=core user=stellar "
-    //                             "password=__PGPASS__ host=localhost"};
-    // cfg->PUBLIC_HTTP_PORT = true;
-    // cfg->COMMANDS.push_back("ll?level=info");
-    // cfg->DESIRED_MAX_TX_PER_LEDGER = 10000;
-    // // cfg->FORCE_SCP = true;
-    // cfg->RUN_STANDALONE = true;
-    // cfg->BUCKET_DIR_PATH = "buckets";
-    // cfg->NETWORK_PASSPHRASE = "Test SDF Network ; September 2015";
-
-    // using namespace std;
-    // const string historyName = "benchmark";
-    // const string historyGetCmd = "cp history/vs/{0} {1}";
-    // const string historyPutCmd = "cp {0} history/vs/{1}";
-    // const string historyMkdirCmd = "mkdir -p history/vs/{0}";
-    // cfg->HISTORY[historyName] = make_shared<HistoryArchive>(
-    //     historyName, historyGetCmd, historyPutCmd, historyMkdirCmd);
-
-    return cfg;
-}
 
 TEST_CASE("stellar-core benchmark's initialization", "[benchmark][initialize][hide]")
 {
     const size_t nAccounts = 1000;
-    std::unique_ptr<Config> cfg = initializeConfig();
+    Config const& cfg = getTestConfig();
     VirtualClock clock(VirtualClock::REAL_TIME);
-    Application::pointer app = Application::create(clock, *cfg, false);
+    Application::pointer app = Application::create(clock, cfg, false);
     app->applyCfgCommands();
     app->start();
 
@@ -60,13 +36,15 @@ TEST_CASE("stellar-core benchmark's initialization", "[benchmark][initialize][hi
 
 TEST_CASE("stellar-core's benchmark", "[benchmark][execute][hide]")
 {
-    const std::chrono::seconds testDuration(3);
+    const std::chrono::seconds testDuration(1);
     const size_t nAccounts = 1000;
     const uint32_t txRate = 100;
+    const uint32_t txPerLedger = 1000;
 
-    std::unique_ptr<Config> cfg = initializeConfig();
+    Config cfg = getTestConfig();
+    cfg.DESIRED_MAX_TX_PER_LEDGER = txPerLedger;
     VirtualClock clock(VirtualClock::REAL_TIME);
-    Application::pointer app = Application::create(clock, *cfg, false);
+    Application::pointer app = Application::create(clock, cfg, false);
     app->applyCfgCommands();
     app->start();
 
